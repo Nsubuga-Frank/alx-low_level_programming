723ebd07ed7886738d20755d31c06f03668834b9
Nsubuga-Frank <nsubugank@gmail.com>
2022-03-24 16:03:55 -0700

    arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,19 +3,15 @@
 
 
 /**
-
-  * _strncat - Concatenates two strings
-
-  * @dest: The destination value
-
-  * @src: The source value
-
-  * @n: The limit of the concatenation
-
+  * *_strncat - Concatenates two strings
   *
-
+  * @dest: The destination value
+  *
+  * @src: The source value
+  *
+  * @n: The limit of the concatenation
+  *
   * Return: A pointer to the resulting string dest
-
   */
 
 char *_strncat(char *dest, char *src, int n)
