c8a267bca639bbeb95e929754711adb7005f2e73
Nsubuga-Frank <nsubugank@gmail.com>
2022-03-25 13:34:23 -0700

    pass

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+ * main - Print "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+	int i = 0;
+
+	char name[] = "_putchar";
+
+
+
+	while (i < 9)
+
+	{
+
+		_putchar(name[i]);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - Prints alphabets 'a - z'
+ *
+ * Return: nothing
+ */
+
+
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+		_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+}
